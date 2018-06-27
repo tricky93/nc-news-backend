1e764e8a65c10505f469f987c6c760670e2b07e0
tricky93 <ptmwalsh1@gmail.com>
2018-06-27 12:41:39 +0100

    cors enabled express app

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = require("express")();
 const bodyParser = require("body-parser");
 const apiRouter = require("./routes/api");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const { badRouteChoice } = require("./controllers");
 
 mongoose.connect(DB_URL).then(() => {
   console.log(`connected to ${DB_URL}`);
 });
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
